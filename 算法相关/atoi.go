f7a213f77e9aecf482d69bea555b0aedc4f84156
caowanyang <zkcaowanyang@gmail.com>
2018-12-07 13:38:03 +0800

    update

--- 算法相关/atoi.go
+++ 算法相关/atoi.go
@@ -2,40 +2,37 @@ package main
 
 import "fmt"
 
-// 出错返回-1
-func atoi(s string)int {
-    if s == "" {
-        return 0
-    }
-    isPlus := true
-    switch s[0] {
-    case '-':
-        isPlus = false
-        s = s[1:]
-    case '+':
-        isPlus = true
-        s = s[1:]
-    default:
-        break
-    }
-    result := 0
-    for _, v := range []byte(s) {
-        if v < '0' || v > '9' {
-            return -1
-        }
-        v -= '0'
-        result = result*10 + int(v)
-    }
-    if !isPlus {
-        result = -result
-    }
-    return result
+// 空值返回0，非法字符串返回-1
+func atoi(s string) int {
+	if s == "" {
+		return 0
+	}
+	isPlus := true
+	switch s[0] {
+	case '-':
+		isPlus = false
+		s = s[1:]
+	case '+':
+		isPlus = true
+		s = s[1:]
+	default:
+		break
+	}
+	result := 0
+	for _, v := range []byte(s) {
+		if v < '0' || v > '9' {
+			return -1
+		}
+		v -= '0'
+		result = result*10 + int(v)
+	}
+	if !isPlus {
+		result = -result
+	}
+	return result
 }
-
 
 func main() {
-    a := "123a"
-    fmt.Println(atoi(a))
+	a := "123a"
+	fmt.Println(atoi(a))
 }
-
-
